44e58f6bcfc3f5910f3fde05474ab1bcc6ed66c5
Chillpill9797 <adityagoswami193@gmail.com>
2024-03-09 01:32:07 +0530

    importing quiz_brain inside ui, passing value of quiz_brain with datatype of QuizBrain and displaying question on screen(get_next_question)

--- ui.py
+++ ui.py
@@ -1,11 +1,12 @@
 from tkinter import *
-from data import question_data
+from quiz_brain import QuizBrain
 # THEME_COLOR = '#375362'
 THEME_COLOR = '#000000'
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz_data = quiz_brain
         self.window = Tk()
         self.window.title('Quiz-Game')
         self.window.config(padx=20, pady=20, background=THEME_COLOR)
@@ -14,7 +15,7 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
         self.canvas = Canvas(height=250, width=300, background="white")
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
-        self.canvas.create_text(100, 130, text=question_data, font=('arial', 20, 'italic'))
+        self.question_text = self.canvas.create_text(100, 130, text="", font=('arial', 20, 'italic'))
 
         true_image = PhotoImage(file="images/true.png")
         self.true_button = Button(image=true_image, highlightthickness=0)
@@ -24,6 +25,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz_data.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
