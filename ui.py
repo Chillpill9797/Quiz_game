14f489d67e0d637b9b883f74e543f3ad9822e731
Chillpill9797 <adityagoswami193@gmail.com>
2024-03-08 13:14:08 +0530

    UI file added

---
+++ ui.py
@@ -0,0 +1,11 @@
+from tkinter import *
+THEME_COLOR = '#375362'
+
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Quiz-Game')
+
+
+        self.window.mainloop()
