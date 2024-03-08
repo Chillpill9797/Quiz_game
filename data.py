d171b9653d434cdf147fc6018122324a455d1143
Chillpill9797 <adityagoswami193@gmail.com>
2024-03-08 09:03:41 +0530

    fetching question data from trivia db api

--- data.py
+++ data.py
@@ -11,4 +11,5 @@ parameters = {
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+question_data = data["results"]
+
